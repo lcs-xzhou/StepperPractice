5f614d029981cbc5e136ef925c42684dd1d38a0b
Xinlin Zhou <XZhou@lcs.on.ca>
2023-11-15 12:18:53 -0500

    Initial Commit

---
+++ StepperPractice/StepperPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperPracticeApp.swift
+//  StepperPractice
+//
+//  Created by Fiona ZHOU on 2023-11-15.
+//
+
+import SwiftUI
+
+@main
+struct StepperPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
